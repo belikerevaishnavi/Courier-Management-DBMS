c1424070fab9b171d6f7d4e4655e139fd31a7b6f
belikerevaishnavi <72008551+belikerevaishnavi@users.noreply.github.com>
2022-11-30 19:20:43 +0530

    Update set.sql

--- set.sql
+++ set.sql
@@ -1,4 +1,4 @@
-#1
+--1 Find the names and id of all employees who are responsible for a current shipment.
 select pd.fname, pd.minit, pd.lname, pd.id
 from personal_details as pd join delivery_staff as ds join tracking as t
 on ds.ds_id = t.ds_id and pd.id = ds.id
@@ -11,19 +11,21 @@ select pd.fname, pd.minit, pd.lname, pd.id
 from tracking as t join office_staff as os join personal_details as pd
 on t.current_ld = os.ld_id and os.id = pd.id;
 
-#2
+--2 Find all the vehicles that are not assigned any shipment in the current order lot.
 select vehicle_no from vehicle
 except
 select v.vehicle_no
 from vehicle as v join orders as o
 on v.from_ld = o.from_ld and v.to_ld = o.to_ld;
 
-#3
+--3 Find local depots which have both shipments to be sent and shipments to be 
+--received
 select from_ld as ld_id from orders
 intersect
 select to_ld from orders;
 
-#4
+--4 Find the names and id of drivers and delivery staff that have a salary greater than or 
+--equal to 15000
 select pd.fname, pd.minit, pd.lname, pd.id
 from personal_details as pd join driver as d
 on pd.id = d.id
