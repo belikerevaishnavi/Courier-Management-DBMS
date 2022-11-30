96d54a5195057d10c69ddc33b8c36a7add3d6f5e
belikerevaishnavi <72008551+belikerevaishnavi@users.noreply.github.com>
2022-11-30 18:53:03 +0530

    Add files via upload

---
+++ trig_cur.sql
@@ -0,0 +1,9 @@
+delimiter $$
+create trigger up_order
+after insert on orders for each row
+begin
+insert into `tracking`(`track_id`,`ship_id`,`vehicle_no`,`current_ld`,`ds_id`)values
+(concat("TR",RIGHT(new.ship_id,3)), new.ship_id, NULL, new.from_ld,NULL);
+end;
+$$
+delimiter ;
