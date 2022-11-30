ce0957b1e71933f12a9fa8f2ffb8d8180c093f8d
belikerevaishnavi <72008551+belikerevaishnavi@users.noreply.github.com>
2022-11-30 19:18:33 +0530

    Update aggregate.sql

--- aggregate.sql
+++ aggregate.sql
@@ -1,21 +1,21 @@
-#1
+--1 Find the average distance from one local depot to all the other local depots.
 select from_ld, avg(distance)
 from vehicle
 group by from_ld;
 
-#2
+--2 Find the oldest employee employed by the courier management. 
 select @oldest:=max(age)
 from personal_details
 where id not like 'US%';
 select * from personal_details 
 where age = @oldest and id not like 'US%';
 
-#3
+--3 Find the number of drivers whose salary is less than 10000
 select count(*)
 from driver
 where salary < 10000;
 
-#4
+--4 Find the number of vehicles that have to travel more than 2000 km.
 select count(*)
 from vehicle
 where distance > 2000;
